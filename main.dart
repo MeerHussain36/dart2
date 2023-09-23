72175113c0afdce269d409ab8994ef665a951515
MACBOOK PRO <macbookpro@MACBOOKs-MacBook-Pro.local>
2023-09-23 18:18:18 +0500

    emoty file

---
+++ main.dart
@@ -0,0 +1,113 @@
+void main() {
+  // var name = ("syed meer hussain");
+  // print(name);
+
+  //  ALL VARIABLE METHODE
+
+  // var Name = ("syed meer hussain");
+  // var f_name = ("syed amir hussain");
+  // var address = ("secter 15/b ");
+  // var number = ("0315.2563894");
+
+  // print(Name);
+  // print(f_name);
+  // print(address);
+  // print(number);
+
+  // BULT IN DATA TYPE
+
+  // int Name = 10;
+  // print(Name);
+
+  // String call = "shahmeer";
+  // print(call);
+
+  // var f_name = "Amir";
+  // print(f_name);
+
+  // double callme = 10.02;
+  // print(callme);
+
+  // bool incollection = true;
+  // print(incollection);
+
+  // CHEAK DATA TYPE
+
+  // var Nmae = true;
+  // print(Nmae);
+  // print(Nmae.runtimeType);
+
+  // ARITHAMATIC OPERATORS COMPLETE
+
+  // var a = 20;
+  // var b = 30;
+  // var c = 40;
+  // var d = 50;
+
+  // print(a - b);
+  // print(a + b);
+  // print(a * b);
+  // print(c / d);
+  // print(c ~/ d);
+  // print(a % b);
+
+  //  PREFIX & POSTFIX INCREMENT AND DECREMENT
+
+//   var i = 3;
+//   print(i);
+//   print(i++);
+//   print(i);
+
+//   var a = 3;
+//   print(a);
+//   print(++a);
+//   print(a);
+
+  // var i = 3;
+  // print(i);
+  // print(i--);
+  // print(i);
+  // var a = 3;
+  // print(a);
+  // print(--a);
+  // print(a);
+
+// EQUILITY AND RELETION OPERATOR
+
+  // var a = 100;
+  // var b = 200;
+
+  // print(a == b);
+  // print(a != b);
+  // print(a > b);
+  // print(a < b);
+  // print(a >= b);
+  // print(a <= b);
+
+  // LOGICAL OPERATORS
+
+  // true && true = true
+  // true && false = false
+  // false && true = false
+  // false && false = false
+
+  // true || true = true
+  // true || false = true
+  // false || true = true
+  // false || false = false
+
+//    EMPLIMENT LOGICAL OPERATOR
+
+  // var a = 100;
+  // var b = 100;
+  // var c = 200;
+  // var d = 200;
+
+  // print((a == b) && (c == d));
+  // print((a == b) || (c == d));
+
+//   TEST TYPE OPERATOR
+
+  
+
+  
